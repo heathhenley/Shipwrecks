c78c2dce8feb9e5eeb69887c4bca23f7f5891ca3
Heath Henley <heath.j.henley@gmail.com>
2022-12-04 15:46:02 -0500

    Adding links to references and updating display

--- wrecks/admin.py
+++ wrecks/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from django.db.models.functions import Lower
 
 from .models import Wreck
-admin.site.register(Wreck)
+
+@admin.register(Wreck)
+class WreckAdmin(admin.ModelAdmin):
+    search_fields = ["vessel_name"]
+
+    def get_ordering(self, request):
+      return [Lower("vessel_name")]
--- wrecks/models.py
+++ wrecks/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class Wreck(models.Model):
-  vessel_name = models.CharField(max_length=200)
+  vessel_name = models.CharField(max_length=200, unique=True)
   # TODO (Heath): This can be a GIS point in a database that supports that, but
   # for now just going to keep it as lat / lon as we're using sqlite.
   # There's only about 150 records in the first version, so no need to go too
   # crazy in IMO.
   latitude = models.FloatField()
   longitude= models.FloatField()
-  year = models.IntegerField(default=None)
-  wreckhunter_link = models.URLField(default='')
+  year = models.IntegerField(default=None, null=True)
+  wreckhunter_link = models.URLField(default='', null=True)
+  beavertail_link = models.URLField(default='', null=True)
 
+  ordering = ['vessel_name']
   def __str__(self):
     return self.vessel_name
