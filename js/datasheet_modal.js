930ea6e6fd385a8ec58413e51780b96679cddb52
Heath Henley <heath.j.henley@gmail.com>
2023-01-05 00:37:17 -0500

    Fix link pass

--- js/datasheet_modal.js
+++ js/datasheet_modal.js
@@ -1,10 +1,11 @@
     // Event handler to open modal / iframe for datasheet
     function openModal() {
-      // TODO (Heath): This is using the global window event, so should add
+      // TODO (Heath): This is using the global window event, instead should add
       // a listener that takes a param to the link tags instead
+      event.preventDefault();
       document.getElementById("popupdarkbg").style.display = "block";
       document.getElementById("popup").style.display = "block";
-      document.getElementById('popupiframe').src = event.target.dataset["url"];
+      document.getElementById('popupiframe').src = event.target["href"];
       document.getElementById('popupdarkbg').onclick = function() {
         document.getElementById("popup").style.display = "none";
         document.getElementById("popupdarkbg").style.display = "none";
