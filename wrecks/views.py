c5ed59252af4a79c898308d3bd717ba1d56d2732
Heath Henley <heath.j.henley@gmail.com>
2023-01-05 01:35:14 -0500

    White space and move wreck parse to loop

--- wrecks/views.py
+++ wrecks/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Wreck
 
 def index(request):
-  context = { 'wrecks_list': Wreck.objects.all() }
+  context = { 'wrecks_list': list(Wreck.objects.all().values()) }
   return render(request, 'wrecks/index.html', context)
